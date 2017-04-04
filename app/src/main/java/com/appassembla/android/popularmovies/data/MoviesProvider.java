584ffed70d2891663ff069b51d547a9eeee3271a
Richard Thompson <richarth@gmail.com>
2017-04-04 19:02:27 +0100

    Finished the content provider

--- app/src/main/java/com/appassembla/android/popularmovies/data/MoviesProvider.java
+++ app/src/main/java/com/appassembla/android/popularmovies/data/MoviesProvider.java
@@ -34,45 +34,12 @@ public class MoviesProvider extends ContentProvider {
         return matcher;
     }
 
-    /**
-     * In onCreate, we initialize our content provider on startup. This method is called for all
-     * registered content providers on the application main thread at application launch time.
-     * It must not perform lengthy operations, or application startup will be delayed.
-     *
-     * Nontrivial initialization (such as opening, upgrading, and scanning
-     * databases) should be deferred until the content provider is used (via {@link #query},
-     * {@link #bulkInsert(Uri, ContentValues[])}, etc).
-     *
-     * Deferred initialization keeps application startup fast, avoids unnecessary work if the
-     * provider turns out not to be needed, and stops database errors (such as a full disk) from
-     * halting application launch.
-     *
-     * @return true if the provider was successfully loaded, false otherwise
-     */
     @Override
     public boolean onCreate() {
-        /*
-         * As noted in the comment above, onCreate is run on the main thread, so performing any
-         * lengthy operations will cause lag in your app. Since WeatherDbHelper's constructor is
-         * very lightweight, we are safe to perform that initialization here.
-         */
         mOpenHelper = new MovieDBHelper(getContext());
         return true;
     }
 
-    /**
-     * Handles requests to insert a set of new rows. In Sunshine, we are only going to be
-     * inserting multiple rows of data at a time from a weather forecast. There is no use case
-     * for inserting a single row of data into our ContentProvider, and so we are only going to
-     * implement bulkInsert. In a normal ContentProvider's implementation, you will probably want
-     * to provide proper functionality for the insert method as well.
-     *
-     * @param uri    The content:// URI of the insertion request.
-     * @param values An array of sets of column_name/value pairs to add to the database.
-     *               This must not be {@code null}.
-     *
-     * @return The number of values that were inserted.
-     */
     @Override
     public int bulkInsert(@NonNull Uri uri, @NonNull ContentValues[] values) {
         final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
@@ -105,82 +72,23 @@ public class MoviesProvider extends ContentProvider {
         }
     }
 
-    /**
-     * Handles query requests from clients. We will use this method in Sunshine to query for all
-     * of our weather data as well as to query for the weather on a particular day.
-     *
-     * @param uri           The URI to query
-     * @param projection    The list of columns to put into the cursor. If null, all columns are
-     *                      included.
-     * @param selection     A selection criteria to apply when filtering rows. If null, then all
-     *                      rows are included.
-     * @param selectionArgs You may include ?s in selection, which will be replaced by
-     *                      the values from selectionArgs, in order that they appear in the
-     *                      selection.
-     * @param sortOrder     How the rows in the cursor should be sorted.
-     * @return A Cursor containing the results of the query. In our implementation,
-     */
     @Override
     public Cursor query(@NonNull Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
 
         Cursor cursor;
 
-        /*
-         * Here's the switch statement that, given a URI, will determine what kind of request is
-         * being made and query the database accordingly.
-         */
         switch (sUriMatcher.match(uri)) {
 
-            /*
-             * When sUriMatcher's match method is called with a URI that looks something like this
-             *
-             *      content://com.example.android.sunshine/weather/1472214172
-             *
-             * sUriMatcher's match method will return the code that indicates to us that we need
-             * to return the weather for a particular date. The date in this code is encoded in
-             * milliseconds and is at the very end of the URI (1472214172) and can be accessed
-             * programmatically using Uri's getLastPathSegment method.
-             *
-             * In this case, we want to return a cursor that contains one row of weather data for
-             * a particular date.
-             */
             case CODE_MOVIES_WITH_ID: {
 
-                /*
-                 * In order to determine the date associated with this URI, we look at the last
-                 * path segment. In the comment above, the last path segment is 1472214172 and
-                 * represents the number of seconds since the epoch, or UTC time.
-                 */
-                String normalizedUtcDateString = uri.getLastPathSegment();
+                String movieId = uri.getLastPathSegment();
 
-                /*
-                 * The query method accepts a string array of arguments, as there may be more
-                 * than one "?" in the selection statement. Even though in our case, we only have
-                 * one "?", we have to create a string array that only contains one element
-                 * because this method signature accepts a string array.
-                 */
-                String[] selectionArguments = new String[]{normalizedUtcDateString};
+                String[] selectionArguments = new String[]{movieId};
 
                 cursor = mOpenHelper.getReadableDatabase().query(
-                        /* Table we are going to query */
                         MovieContract.MovieEntry.TABLE_NAME,
-                        /*
-                         * A projection designates the columns we want returned in our Cursor.
-                         * Passing null will return all columns of data within the Cursor.
-                         * However, if you don't need all the data from the table, it's best
-                         * practice to limit the columns returned in the Cursor with a projection.
-                         */
                         projection,
-                        /*
-                         * The URI that matches CODE_WEATHER_WITH_DATE contains a date at the end
-                         * of it. We extract that date and use it with these next two lines to
-                         * specify the row of weather we want returned in the cursor. We use a
-                         * question mark here and then designate selectionArguments as the next
-                         * argument for performance reasons. Whatever Strings are contained
-                         * within the selectionArguments array will be inserted into the
-                         * selection statement by SQLite under the hood.
-                         */
                         MovieContract.MovieEntry.COLUMN_MOVIE_ID + " = ? ",
                         selectionArguments,
                         null,
@@ -190,17 +98,6 @@ public class MoviesProvider extends ContentProvider {
                 break;
             }
 
-            /*
-             * When sUriMatcher's match method is called with a URI that looks EXACTLY like this
-             *
-             *      content://com.example.android.sunshine/weather/
-             *
-             * sUriMatcher's match method will return the code that indicates to us that we need
-             * to return all of the weather in our weather table.
-             *
-             * In this case, we want to return a cursor that contains every row of weather data
-             * in our weather table.
-             */
             case CODE_MOVIES: {
                 cursor = mOpenHelper.getReadableDatabase().query(
                         MovieContract.MovieEntry.TABLE_NAME,
@@ -222,27 +119,12 @@ public class MoviesProvider extends ContentProvider {
         return cursor;
     }
 
-    /**
-     * Deletes data at a given URI with optional arguments for more fine tuned deletions.
-     *
-     * @param uri           The full URI to query
-     * @param selection     An optional restriction to apply to rows when deleting.
-     * @param selectionArgs Used in conjunction with the selection statement
-     * @return The number of rows deleted
-     */
     @Override
     public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
 
         /* Users of the delete method will expect the number of rows deleted to be returned. */
         int numRowsDeleted;
 
-        /*
-         * If we pass null as the selection to SQLiteDatabase#delete, our entire table will be
-         * deleted. However, if we do pass null and delete all of the rows in the table, we won't
-         * know how many rows were deleted. According to the documentation for SQLiteDatabase,
-         * passing "1" for the selection will delete all rows and return the number of rows
-         * deleted, which is what the caller of this method expects.
-         */
         if (null == selection) selection = "1";
 
         switch (sUriMatcher.match(uri)) {
@@ -267,48 +149,22 @@ public class MoviesProvider extends ContentProvider {
         return numRowsDeleted;
     }
 
-    /**
-     * In Sunshine, we aren't going to do anything with this method. However, we are required to
-     * override it as WeatherProvider extends ContentProvider and getType is an abstract method in
-     * ContentProvider. Normally, this method handles requests for the MIME type of the data at the
-     * given URI. For example, if your app provided images at a particular URI, then you would
-     * return an image URI from this method.
-     *
-     * @param uri the URI to query.
-     * @return nothing in Sunshine, but normally a MIME type string, or null if there is no type.
-     */
     @Override
     public String getType(@NonNull Uri uri) {
-        throw new RuntimeException("We are not implementing getType in Sunshine.");
+        throw new RuntimeException("We are not implementing getType in Popular Movies.");
     }
 
-    /**
-     * In Sunshine, we aren't going to do anything with this method. However, we are required to
-     * override it as WeatherProvider extends ContentProvider and insert is an abstract method in
-     * ContentProvider. Rather than the single insert method, we are only going to implement
-     * {@link #bulkInsert}.
-     *
-     * @param uri    The URI of the insertion request. This must not be null.
-     * @param values A set of column_name/value pairs to add to the database.
-     *               This must not be null
-     * @return nothing in Sunshine, but normally the URI for the newly inserted item.
-     */
     @Override
     public Uri insert(@NonNull Uri uri, ContentValues values) {
         throw new RuntimeException(
-                "We are not implementing insert in Sunshine. Use bulkInsert instead");
+                "We are not implementing insert in Popular Movies. Use bulkInsert instead");
     }
 
     @Override
     public int update(@NonNull Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        throw new RuntimeException("We are not implementing update in Sunshine");
+        throw new RuntimeException("We are not implementing update in Popular Movies");
     }
 
-    /**
-     * You do not need to call this method. This is a method specifically to assist the testing
-     * framework in running smoothly. You can read more at:
-     * http://developer.android.com/reference/android/content/ContentProvider.html#shutdown()
-     */
     @Override
     @TargetApi(11)
     public void shutdown() {
